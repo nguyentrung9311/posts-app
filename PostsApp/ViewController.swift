d86bdd14ad8912173837589a0a6c15c6b9ee5ed4
TrungNV (Macbook) <trungnv@ecomobile.vn>
2023-06-06 21:08:30 +0700

    Init project

---
+++ PostsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsApp
+//
+//  Created by TrungNV (Macbook) on 06/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
